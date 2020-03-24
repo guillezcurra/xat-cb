fb6e4db38691f2c0e1c3d3d260488a2a5d99575b
guillezcurra <guillezcurra@gmail.com>
2020-03-24 16:11:42 +0000

    docker

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
 import os
-
+from channels.asgi import get_channel_layer
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_asgi_application()
+
+channel_layer = get_channel_layer()
