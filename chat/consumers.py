65002486ee4051456268c7628b35683c611b364c
guillezcurra <guillezcurra@gmail.com>
2020-03-24 23:14:17 +0000

    syncronous room

--- chat/consumers.py
+++ chat/consumers.py
@@ -39,7 +39,16 @@ class ChatConsumer(WebsocketConsumer):
                 'message': message
             }
         )
-
+    def receive_json(self, content):
+        text_data_json = json.loads(content)
+        message = text_data_json['message']
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
     # Receive message from room group
     def chat_message(self, event):
         message = event['message']
