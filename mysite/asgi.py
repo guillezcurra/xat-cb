1eeed28f562740f2f8658504d2de16b3c539f687
guillezcurra <guillezcurra@gmail.com>
2020-03-25 16:15:05 +0000

    froçar https

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -8,11 +8,12 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
 import os
-from channels.layers import get_channel_layer
-from django.core.asgi import get_asgi_application
+import django
+from channels.routing import get_default_application
+
+#from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+django.setup()
 
-application = get_asgi_application()
-
-channel_layer = get_channel_layer()
+application = get_default_application()
