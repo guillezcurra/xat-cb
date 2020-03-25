134fc1f9177eb9074c1989bced0fe47031df3194
guillezcurra <guillezcurra@gmail.com>
2020-03-25 13:40:56 +0000

    canvis certificat

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,12 +121,12 @@ USE_TZ = True
 redis_host = os.environ.get('REDIS_HOST', 'localhost')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 ASGI_APPLICATION = 'mysite.routing.application'
 # mysite/settings.py
 # Channels
