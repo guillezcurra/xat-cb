837c4133db19560e81c164a4895b289faf62661d
guillezcurra <guillezcurra@gmail.com>
2020-03-25 15:59:59 +0000

    froçar https

--- chat/routing.py
+++ chat/routing.py
@@ -4,6 +4,6 @@ from django.conf.urls import url
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'chat/(?P<room_name>\w+)/', consumers.ChatConsumer),
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer),
     # re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer),
 ]
