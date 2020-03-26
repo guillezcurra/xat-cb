33cbce937024668e496c359920d0e52656f68896
guillezcurra <guillezcurra@gmail.com>
2020-03-26 17:13:28 +0000

    Arreglar la plantilla

--- chat/consumers.py
+++ chat/consumers.py
@@ -34,6 +34,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             {
                 'type': 'chat_message',
+                "username": self.scope["user"].username,
                 'message': message
             }
         )
@@ -41,8 +42,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
     # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
-
+        username = event['username']
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'username' : username
         }))
